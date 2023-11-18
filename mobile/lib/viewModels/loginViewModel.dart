b75d1a6100937a4e7bbac80e72aed4a4dc521375
Paweł Budnik <82655@student.pb.edu.pl>
2023-11-18 19:06:05 +0100

    [PZGRAP-51] Dodanie loginViewModel

---
+++ mobile/lib/viewModels/loginViewModel.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class loginViewModel{
+  loginViewModel();
+
+  String _username = "";
+  String _password = "";
+
+  String get username => _username;
+  String get password => _password;
+
+  void setUsername(String username){
+    _username = username;
+  }
+
+  void setPassword(String password){
+    _password = password;
+  }
+
+}
