ee86287d1717754c588d5dbee47c1a3d342383ee
Paweł Budnik <82655@student.pb.edu.pl>
2023-11-21 18:31:47 +0100

    [PZGRAP-52] Dodanie registerViewModel

---
+++ mobile/lib/viewModels/registerViewModel.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+
+class registerViewModel with ChangeNotifier {
+
+  final _myRepo = AccountService();
+
+  bool _loading = false;
+  bool get loading => _loading;
+
+  setLoading(bool value){
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> createAccount (dynamic data, BuildContext context) async {
+
+    setLoading(true);
+
+    _myRepo.registerApi(data).then((value){
+      setLoading(false);
+      //  nawigacja do ekranu głównego
+      // Navigator.pushNamed(context, RoutesName.home);
+      if (kDebugMode){
+        print(value.toString());
+      }
+
+    }).onError((error, stackTrace) {
+      setLoading(false);
+      if(kDebugMode){
+        print(error.toString());
+      }
+    });
+  }
+}
