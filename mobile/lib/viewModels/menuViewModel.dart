49ef651a9b9ff887d93cd25ad8df5fb0f9acc233
Paweł Budnik <82655@student.pb.edu.pl>
2023-11-28 19:06:57 +0100

    [PZGRAP-65] Dodadnie notifyListeners

--- mobile/lib/viewModels/menuViewModel.dart
+++ mobile/lib/viewModels/menuViewModel.dart
@@ -1,63 +1,65 @@
 import 'package:flutter/material.dart';
 
-class MenuViewModel extends ChangeNotifier{
-
+class MenuViewModel extends ChangeNotifier {
   String _nickname = "Testowy123";
 
   String get nickname => _nickname;
 
   void joinGame(BuildContext context) {
-    /* przejście do widoku dołączania do poczekalni
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => joinGameView());
-     */
+    // Przejście do widoku dołączania do poczekalni
+    /*Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => JoinGameView()),
+    );*/
 
+    // Powiadomienie listenerów o zmianie stanu
+    notifyListeners();
   }
 
   void createGame(BuildContext context) {
-    /* przejście do widoku tworzenia poczekalni
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => createGameView());
-
-    tymczasowo (?) przejście do widoku poczekalni
-    zakomentowanie bo nie ma merga z widokiem poczekalni
-
-    Navigator.push(
+    // Przejście do widoku tworzenia poczekalni
+    /*Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => WaitingRoom());
-    )
+      MaterialPageRoute(builder: (context) => CreateGameView()),
+    );*/
 
-     */
+    // Tymczasowo (?) przejście do widoku poczekalni
+    // Zakomentowane, bo nie ma merga z widokiem poczekalni
+    // Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => WaitingRoom()),
+    // );
 
+    // Powiadomienie listenerów o zmianie stanu
+    notifyListeners();
   }
 
   void gameHistory(BuildContext context) {
-    /* przejście do widoku z historią gier
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => gameHistoryView());
-     */
+    // Przejście do widoku z historią gier
+   /* Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GameHistoryView()),
+    );*/
 
+    // Powiadomienie listenerów o zmianie stanu
+    notifyListeners();
   }
 
   void settings(BuildContext context) {
-    /* przejście do widoku ustawień
-    Navigator.push(
-    context,
-    MaterialPageRoute(builder: (context) => creatingGameView());
-     */
+    // Przejście do widoku ustawień
+   /* Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => SettingsView()),
+    );*/
 
+    // Powiadomienie listenerów o zmianie stanu
+    notifyListeners();
   }
 
   void logout() {
-    /*
+    // Tutaj logika do wylogowywania się
 
-    tutaj logika do wylogowywania się
-
-     */
-
+    // Powiadomienie listenerów o zmianie stanu
+    notifyListeners();
   }
-
 }
