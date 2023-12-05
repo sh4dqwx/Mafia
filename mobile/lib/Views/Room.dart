a196782e64008a4b1fe0b14137f1ea17e2a93918
Aleksandra Walendziuk <82960@student.pb.edu.pl>
2023-12-05 21:12:48 +0100

    [PZGRAP-78] RoomPage jako jedno bez tego zagniezdzania,odwolania do viewmodela ktorego na razie nia ma (Stad bledy)

--- mobile/lib/Views/Room.dart
+++ mobile/lib/Views/Room.dart
@@ -1,114 +1,92 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:provider/provider.dart';
 
-class Room extends StatelessWidget {
+class RoomPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WaitingRoom(),
-    );
-  }
-}
-
-class WaitingRoom extends StatefulWidget {
-  @override
-  _WaitingRoomState createState() => _WaitingRoomState();
-}
-
-class _WaitingRoomState extends State<WaitingRoom> {
-  int numberOfPlayers = 0;
-  String roomCode = generateRandomCode();
-  bool isHost = false; // czy użytkownik jest hostem
-
-  static String generateRandomCode() {
-    final Random random = Random();
-    const String chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    return List.generate(4, (index) => chars[random.nextInt(chars.length)]).join();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Room'),
-        backgroundColor: Colors.blue,
-        actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: GestureDetector(
-                onTap: () {
-                  // ustawienia dla wszysktich
-                  print('Open settings');
-                },
-                child: Icon(
-                  Icons.settings,
-                  size: 30,
-                ),
-              ),
-            ),
-        ],
-      ),
-      body: Stack(
-        children: [
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Number of Players: $numberOfPlayers',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 20),
-                ElevatedButton(
-                  onPressed: () {
-                    print('Start the game');
-                  },
-                  child: Text('Start the game'),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.orange,
-                    onPrimary: Colors.white,
-                    textStyle: TextStyle(fontSize: 18),
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  ),
-                ),
-                SizedBox(height: 10),
-                if (isHost)
-                  ElevatedButton(
-                    onPressed: () {
-                      // ustawienia gry
-                      print('Open game settings');
+    return ChangeNotifierProvider(
+      create: (context) => RoomViewModel(),
+      child: Consumer<RoomViewModel>(
+        builder: (context, viewModel, child) {
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Room'),
+              backgroundColor: Colors.blue,
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      print('Open settings');
                     },
-                    child: Text('Game Settings'),
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.blue,
-                      onPrimary: Colors.white,
-                      textStyle: TextStyle(fontSize: 18),
-                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: Icon(
+                      Icons.settings,
+                      size: 30,
                     ),
                   ),
-                SizedBox(height: 20),
-                Text('Code: $roomCode'),
-                if (isHost)
-                  Text('You are the host'),
+                ),
               ],
             ),
-          ),
-          Positioned(
-            top: 20,
-            right: 20,
-            child: GestureDetector(
-              onTap: () {
-                // otwieranie chatu
-                print('Open chat');
-              },
-              child: Icon(
-                Icons.chat,
-                size: 40,
-                color: Colors.blue,
-              ),
+            body: Stack(
+              children: [
+                Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Number of Players: ${viewModel.numberOfPlayers}',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 20),
+                      ElevatedButton(
+                        onPressed: viewModel.startGame,
+                        child: Text('Start the game'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.orange,
+                          onPrimary: Colors.white,
+                          textStyle: TextStyle(fontSize: 18),
+                          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      if (viewModel.isHost)
+                        ElevatedButton(
+                          onPressed: viewModel.openGameSettings,
+                          child: Text('Game Settings'),
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.blue,
+                            onPrimary: Colors.white,
+                            textStyle: TextStyle(fontSize: 18),
+                            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                          ),
+                        ),
+                      SizedBox(height: 20),
+                      Text('Code: ${viewModel.roomCode}'),
+                      if (viewModel.isHost)
+                        Text('You are the host'),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 20,
+                  right: 20,
+                  child: GestureDetector(
+                    onTap: () {
+                      // Otwieranie chatu
+                      print('Open chat');
+                    },
+                    child: Icon(
+                      Icons.chat,
+                      size: 40,
+                      color: Colors.blue,
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
