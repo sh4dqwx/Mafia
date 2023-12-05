158b06107230fce053d7e03f4ae6a0c1941d2a04
Aleksandra Walendziuk <82960@student.pb.edu.pl>
2023-12-05 08:56:09 +0100

    [PZGRAP-73] Widok dołączania do poczekalni prywatnej

---
+++ mobile/lib/Views/JoinPrivateRoom.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class JoinPrivateRoom extends StatelessWidget {
+  TextEditingController lobbyCodeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Join Room'),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: TextField(
+                controller: lobbyCodeController,
+                decoration: InputDecoration(
+                  hintText: 'Enter the Room Code',
+                  border: InputBorder.none,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String lobbyCode = lobbyCodeController.text;
+                if (lobbyCode.isNotEmpty) {
+                  //ekran poczekalni
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Please enter the Room Code'),
+                    ),
+                  );
+                }
+              },
+              child: Text('Join'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
