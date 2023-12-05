3462bdf08dd34b55090470fff1b04aeadb2300ef
Paweł Budnik <82655@student.pb.edu.pl>
2023-12-05 20:57:52 +0100

    [PZGRAP-74] Stworzenie viewmodelu dla listy publicznych pokoi

---
+++ mobile/lib/viewModels/PublicRoomsListViewModel.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile/models/Room.dart';
+import 'package:mobile/services/network/RoomService.dart';
+
+class PublicRoomsViewModel with ChangeNotifier {
+  final RoomService _roomService = RoomService();
+  List<Room> _publicRooms = [];
+
+  List<Room> get publicRooms => _publicRooms;
+
+  Future<void> fetchPublicRooms() async {
+    try {
+      List<Room> pokoje = await _roomService.getAllRooms();
+      _publicRooms = pokoje;
+      notifyListeners();
+    } catch (e) {
+      print('Błąd podczas pobierania listy pokoi: $e');
+    }
+  }
+
+  void pressed(BuildContext context, Room room) {
+    /* Przekierowanie do pokoju
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Room(room: room),
+      ),
+    );*/
+  }
+}
