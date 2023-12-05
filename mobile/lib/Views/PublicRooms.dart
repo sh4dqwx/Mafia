a788c054e7f8baf628d438b4a3d7980226bec991
Paweł Niewierowski <82843@student.pb.edu.pl>
2023-12-05 19:52:21 +0100

    [PZGRAP-60] Dodano widok do publicznych pokoi

---
+++ mobile/lib/Views/PublicRooms.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '../models/Room.dart';
+
+class PublicRoomsPage extends StatefulWidget {
+  const PublicRoomsPage({super.key});
+
+  @override
+  _PublicRoomsPageState createState() => _PublicRoomsPageState();
+}
+
+class _PublicRoomsPageState extends State<PublicRoomsPage> {
+  @override
+  Widget build(BuildContext context) {
+    //List<Room> publicRooms = context.watch<PublicRoomsViewModel>().getPublicRooms();
+    List<Room> publicRooms = List.empty();
+
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Public Rooms'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: ListView(
+          children: [
+            const SizedBox(height: 25.0),
+            const Text(
+              'Choose a Public Room',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 25.0),
+            for (Room room in publicRooms)
+              ElevatedButton(
+                onPressed: () {
+                  //context.read<PublicRoomsViewModel>().pressed(room);
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 60),
+                  textStyle: const TextStyle(fontSize: 32.0),
+                ),
+                child: Text(room.id.toString()),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
