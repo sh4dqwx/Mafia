8994d49fb771691afe3df4875c18576dea0c322c
Aleksandra Walendziuk <82960@student.pb.edu.pl>
2023-12-06 13:00:14 +0100

    [PZGRAP-73] widok z viewmodelem jest

--- mobile/lib/Views/JoinPrivateRoom.dart
+++ mobile/lib/Views/JoinPrivateRoom.dart
@@ -7,37 +7,37 @@ class JoinPrivateRoom extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Join Room'),
+        title: const Text('Join Room'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(10.0),
+        padding: const EdgeInsets.all(10.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
               decoration: BoxDecoration(
                 border: Border.all(color: Colors.grey),
                 borderRadius: BorderRadius.circular(8.0),
               ),
               child: TextField(
                 controller: lobbyCodeController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   hintText: 'Enter the Room Code',
                   border: InputBorder.none,
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
                 String lobbyCode = lobbyCodeController.text;
-                if (lobbyCode.isNotEmpty) {
-                  //ekran poczekalni o danym kodzie
+                if (JoinPrivateRoomViewModel.validateLobbyCode(lobbyCode)) {
+                  JoinPrivareRoomViewModel.join(lobbyCode);
                 } else {
                   ScaffoldMessenger.of(context).showSnackBar(
                     SnackBar(
-                      content: Text('Please enter the Room Code'),
+                      content: Text('Please enter the correct Room Code'),
                     ),
                   );
                 }
