1238cade439307d5ece6a79d16a7a646b2654c3c
Paweł Niewierowski <82843@student.pb.edu.pl>
2023-12-09 00:11:25 +0100

    [PZGRAP-91-94-97] Ujednolicono mapowanie, stworzono endpoint do logowania i poprawiono endpoint do tworzenia konta. Dodatkowo poprawiono nazewnictwo metod.

--- backend/src/main/java/pl/mafia/backend/controllers/AccountController.java
+++ backend/src/main/java/pl/mafia/backend/controllers/AccountController.java
@@ -9,52 +9,76 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/account")
 public class AccountController {
 
     @Autowired
     private AccountRepository accountRepository;
 
-    @GetMapping("/account")
+    @GetMapping()
     public List<Account> getAllAccounts(){
-        return (List<Account>) accountRepository.findAll();
+        return accountRepository.findAll();
     }
-    @GetMapping("/account/{id}")
-    public Account getAccount(@PathVariable String id){
+
+    @GetMapping("/{id}")
+    public Account getAccountById(@PathVariable String id){
         long accountId = Long.parseLong(id);
         return accountRepository.findById(accountId);
     }
 
-    @DeleteMapping("/account/{id}")
-    public void deleteAccount(@PathVariable String id) {
+    @DeleteMapping("/{id}")
+    public void deleteAccountById(@PathVariable String id) {
         long accountId = Long.parseLong(id);
         Account account = accountRepository.findById(accountId);
         if (account == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account does not exists.");
         } else {
             accountRepository.delete(account);
         }
     }
-    @PostMapping("/account")
-    public Account createAccount(@RequestBody Account account) {
-        String login = account.getLogin();
-        String email = account.getEmail();
-
+    @PostMapping()
+    public Account createAccount(@RequestBody String email, @RequestBody String login, @RequestBody String password) {
         Account existingLogin = accountRepository.findByLogin(login);
         Account existingEmail = accountRepository.findByEmail(email);
+
         if (existingLogin != null || existingEmail != null) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Account already exists.");
         }
+
+        //hashowanie hasła?
+
+        Account account = new Account();
+        account.setEmail(email);
+        account.setLogin(login);
+        account.setPassword(password);
+        account.setNickname(login);
+
         return accountRepository.save(account);
     }
 
-    @PutMapping("/account/{id}")
-    public Account updateAccount(@RequestBody Account account, @PathVariable String id) {
+    @PutMapping("/{id}")
+    public Account updateAccountById(@RequestBody Account account, @PathVariable String id) {
         long accountId = Long.parseLong(id);
         Account existingAccount = accountRepository.findById(accountId);
         if (existingAccount == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account does not exists.");
         } else {
             return accountRepository.save(account);
         }
+    }
+
+    @PostMapping("/login")
+    public Account loginToAccount(@RequestBody String login, @RequestBody String password) {
+        Account existingAccount = accountRepository.findByLogin(login);
+
+        if (existingAccount == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Account does not exists.");
+        }
+
+        if (!existingAccount.getPassword().equals(password)) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Wrong password.");
+        }
+
+        return existingAccount;
     }
 }
