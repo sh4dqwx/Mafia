55353c4458e1c8d1b07c0c0ebdfcabfda19b0b1f
Paweł Budnik <82655@student.pb.edu.pl>
2023-12-10 13:26:02 +0100

    [PZGRAP-79] Usunięcie klasy RoomSettings, zmiana nazwy z RoomSettingsState na RoomSettingsPageState

--- mobile/lib/Views/RoomSettings.dart
+++ mobile/lib/Views/RoomSettings.dart
@@ -1,24 +1,11 @@
 import 'package:flutter/material.dart';
 
-class RoomSettings extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Menu',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: RoomSettingsPage(),
-    );
-  }
-}
-
 class RoomSettingsPage extends StatefulWidget {
   @override
-  _RoomSettingsState createState() => _RoomSettingsState();
+  _RoomSettingsPageState createState() => _RoomSettingsPageState();
 }
 
-class _RoomSettingsState extends State<RoomSettingsPage> {
+class _RoomSettingsPageState extends State<RoomSettingsPage> {
   bool _isPublic = true; // Default to public room
   int _numberOfPlayers = 6; // Default number of players
 
@@ -82,7 +69,7 @@ class _RoomSettingsState extends State<RoomSettingsPage> {
             SizedBox(height: 20.0),
             ElevatedButton(
               // komentarz bo niepołączone branche
-              onPressed: () => {}, //_viewModel.createGame(context),
+              onPressed: () => {}, //_viewModel.saveGameSettings(context),
               style: ElevatedButton.styleFrom(
                 minimumSize: Size(double.infinity, 60),
                 textStyle: TextStyle(fontSize: 32.0),
