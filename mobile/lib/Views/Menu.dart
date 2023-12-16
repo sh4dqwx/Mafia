1b5de18442c77233744b3433a6398f5fba580b9f
Paweł Budnik <82655@student.pb.edu.pl>
2023-12-16 18:36:27 +0100

    [PZGRAP-111] Zablokowanie działania guzika od cofania na ekranie menu

--- mobile/lib/Views/Menu.dart
+++ mobile/lib/Views/Menu.dart
@@ -16,99 +16,102 @@ class MenuPage extends StatefulWidget {
 class MenuPageState extends State<MenuPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: const Text('Menu')
-      ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 25.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 25.0),
-            const Text(
-                'Welcome!',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: AppBar(
+              automaticallyImplyLeading: false, title: const Text('Menu')),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 25.0),
+                const Text(
+                  'Welcome!',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-            const SizedBox(height: 25.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => JoinPrivateRoomPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(MediaQuery.of(context).size.width - 25.0, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Enter room code'),
+                const SizedBox(height: 25.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => JoinPrivateRoomPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize:
+                        Size(MediaQuery.of(context).size.width - 25.0, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Enter room code'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<MenuViewModel>().showPublicRoomsList(context);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PublicRoomsPage()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize:
+                        Size(MediaQuery.of(context).size.width - 25.0, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Public rooms list'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () async {},
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Create new room'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<MenuViewModel>().gameHistory(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Game history'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    context.read<MenuViewModel>().settings(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Settings'),
+                ),
+                const SizedBox(height: 20.0),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(double.infinity, 60),
+                    textStyle: const TextStyle(fontSize: 32.0),
+                  ),
+                  child: const Text('Log out'),
+                ),
+              ],
             ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                context.read<MenuViewModel>().showPublicRoomsList(context);
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PublicRoomsPage()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: Size(MediaQuery.of(context).size.width - 25.0, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Public rooms list'),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () async {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Create new room'),
-            ),
-
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                context.read<MenuViewModel>().gameHistory(context);
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Game history'),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                context.read<MenuViewModel>().settings(context);
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Settings'),
-            ),
-            const SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 60),
-                textStyle: const TextStyle(fontSize: 32.0),
-              ),
-              child: const Text('Log out'),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
