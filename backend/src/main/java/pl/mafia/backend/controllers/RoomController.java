c76012fb600dd02686df0842ac011669c31d8147
Paweł Niewierowski <82843@student.pb.edu.pl>
2023-12-19 19:32:53 +0100

    [PZGRAP-99] Dodano endpoint i serwis do obsługi zmiany ustawień pokoju. Dodatkowo stworzono model do ustawień.

--- backend/src/main/java/pl/mafia/backend/controllers/RoomController.java
+++ backend/src/main/java/pl/mafia/backend/controllers/RoomController.java
@@ -3,6 +3,7 @@ package pl.mafia.backend.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import pl.mafia.backend.models.RoomSettings;
 import pl.mafia.backend.repositories.RoomRepository;  // Assuming you have a RoomRepository
 import pl.mafia.backend.models.Room;  // Update import to Room
 import org.springframework.web.bind.annotation.*;
@@ -67,6 +68,17 @@ public class RoomController {
         } catch(IllegalArgumentException ex) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage());
         } catch(Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    @PutMapping("/properties")
+    public void updateProperties(@RequestBody RoomSettings roomSettings, @PathVariable String id) {
+        try {
+            roomService.updateProperties(roomSettings, id);
+        } catch (IllegalArgumentException ex) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+        } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
         }
     }
