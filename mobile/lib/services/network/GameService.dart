50921f361e84b48bd99a4f67c0b48363118949ab
Paweł Budnik <82655@student.pb.edu.pl>
2024-01-29 22:24:26 +0100

    Rozbudowanie startGame w serwisie GameService.dart

--- mobile/lib/services/network/GameService.dart
+++ mobile/lib/services/network/GameService.dart
@@ -1,8 +1,30 @@
-class GameService
-{
-  Future<void> startGame() async
+import 'dart:html';
+import 'dart:io';
+
+import 'package:mobile/services/WebSocketClient.dart';
+import 'package:mobile/utils/Constants.dart' as Constants;
+import 'package:http/http.dart' as http;
+import 'package:mobile/utils/CustomHttpClient.dart';
+import 'package:mobile/utils/NetworkUtils.dart';
+import 'package:mobile/services/network/NetworkException.dart';
+class GameService {
+  final String baseUrl = "http://${Constants.baseUrl}";
+  final CustomHttpClient httpClient = CustomHttpClient();
+  final WebSocketClient webSocketClient = WebSocketClient();
+
+
+  Future<void> startGame(int roomId) async
   {
-
+    try {
+      final response = await httpClient.post(
+          Uri.parse("$baseUrl/game/start/${roomId}"));
+      return handleResponse(response);
+    } catch (e) {
+      if (e is SocketException) {
+        throw FetchDataException('No Internet Connection');
+      } else {
+        rethrow;
+      }
+    }
   }
-
 }
