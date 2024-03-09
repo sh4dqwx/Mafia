fbe90099260ea9871a5cccf7e16ecab373bc54d4
Mariusz Aniszczuk <80250@student.pb.edu.pl>
2024-03-09 18:26:13 +0100

    [PZGRAP-129] Stworzono wyświetlanie roli

--- mobile/lib/views/UserRole.dart
+++ mobile/lib/views/UserRole.dart
@@ -1,35 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/views/Voting.dart';
 import 'package:provider/provider.dart';
 import 'package:mobile/viewModels/WinnerRoleViewModel.dart';
 import 'package:mobile/models/Room.dart';
 
 class UserRolePage extends StatefulWidget {
-  // final Room room;
-  //
-  // UserRolePage(this.room);
-
   @override
   _UserRolePageState createState() => _UserRolePageState();
 }
 
 class _UserRolePageState extends State<UserRolePage> {
-  String _role = "City";
-
-  // final WinnerRoleViewModel viewModel = context.read<WinnerRoleViewModel>();
 
   @override
   Widget build(BuildContext context) {
-
-      return Container(
-        alignment: Alignment.center,
-        child: Text(
-          'Your role is:${_role} ',
-              // '${context
-              // .watch<WinnerRoleViewModel>()
-              // .userRole}',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Consumer<WinnerRoleViewModel>(
+              builder: (context, winnerRoleViewModel, child) {
+                return Text(
+                  'Your role is: '
+                  '${context.watch<WinnerRoleViewModel>().userRole}',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                );
+              },
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => VotingPage()),
+                );
+              },
+              child: Text("Głosowanie"),
+            ),
+          ],
         ),
-      );
-
+      ),
+    );
   }
 }
