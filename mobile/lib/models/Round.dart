a24cd9d255d808c6f55cd7255c6a66e50c29f3e8
xShyQx <93669491+xShyQx@users.noreply.github.com>
2024-03-12 00:40:41 +0100

    Dodalem do WebSocketClient mozliwosc zapisu ostatnio odebranej wiadomosci, dzieki czemu te dane beda zawsze dostepne globalnie w aplikacji, stworzylem obiekt RoundDTO do wysylania z serwera i subskrypcje po stronie aplikacji na topic/roomId/round-start na wysylanie tej informacji

--- mobile/lib/models/Round.dart
+++ mobile/lib/models/Round.dart
@@ -1,48 +1,23 @@
-import 'Game.dart';
-import 'Reward.dart';
-import 'RoundMiniGame.dart';
-import 'Voting.dart';
-
 class Round {
   final int id;
-  final Game game;
-  final Reward reward;
-  final Voting votingCity;
-  final Voting votingMafia;
-  final Voting votingReward;
-  final List<RoundMiniGame> roundMiniGames;
+  final int votingCityId;
 
   Round({
     required this.id,
-    required this.game,
-    required this.reward,
-    required this.votingCity,
-    required this.votingMafia,
-    required this.votingReward,
-    required this.roundMiniGames,
+    required this.votingCityId
   });
 
   factory Round.fromJson(Map<String, dynamic> json) {
     return Round(
       id: json['id'],
-      game: Game.fromJson(json['game'] ?? {}),
-      reward: Reward.fromJson(json['reward'] ?? {}),
-      votingCity: Voting.fromJson(json['votingCity'] ?? {}),
-      votingMafia: Voting.fromJson(json['votingMafia'] ?? {}),
-      votingReward: Voting.fromJson(json['votingReward'] ?? {}),
-      roundMiniGames: List<RoundMiniGame>.from((json['roundMiniGames'] ?? []).map((miniGameJson) => RoundMiniGame.fromJson(miniGameJson))),
+      votingCityId: json['votingCityId']
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'id': id,
-      'game': game.toJson(),
-      'reward': reward.toJson(),
-      'votingCity': votingCity.toJson(),
-      'votingMafia': votingMafia.toJson(),
-      'votingReward': votingReward.toJson(),
-      'roundMiniGames': List<dynamic>.from(roundMiniGames.map((miniGame) => miniGame.toJson())),
+      'votingCityId': votingCityId
     };
   }
 }
